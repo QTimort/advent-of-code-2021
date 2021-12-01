2031a699d069da17c2d407d6b81fc46bd96054d6
Timothy Diguiet <timothy.diguiet@epitech.eu>
2021-12-02 00:51:04 +0100

    Add day1 subject

--- src/main/java/com/diguiet/adventofcode2021/day1/Day1.java
+++ src/main/java/com/diguiet/adventofcode2021/day1/Day1.java
@@ -1,10 +1,21 @@
 package com.diguiet.adventofcode2021.day1;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
+
+import java.nio.charset.Charset;
+import java.util.Objects;
 
 @Slf4j
 public class Day1 {
+    @SneakyThrows
     public static void main(String[] args) {
-        log.info("test");
+        final String input = IOUtils.toString(
+                Objects.requireNonNull(Day1.class.getClassLoader().getResourceAsStream("com/diguiet/adventofcode2021/day1/input.txt")),
+                Charset.defaultCharset()
+        );
+        log.info(input);
     }
+
 }
