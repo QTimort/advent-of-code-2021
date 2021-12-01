2bebf23c572bf16a05afa17b758b0c91628ea857
Timothy Diguiet <timothy.diguiet@epitech.eu>
2021-12-02 00:16:25 +0100

    Add base project

---
+++ src/main/java/com/diguiet/adventofcode2021/day1/Day1.java
@@ -0,0 +1,10 @@
+package com.diguiet.adventofcode2021.day1;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Day1 {
+    public static void main(String[] args) {
+        log.info("test");
+    }
+}
---
+++ src/main/java/com/diguiet/adventofcode2021/day2/Day2.java
@@ -0,0 +1,10 @@
+package com.diguiet.adventofcode2021.day2;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Day2 {
+    public static void main(String[] args) {
+        log.info("test");
+    }
+}
