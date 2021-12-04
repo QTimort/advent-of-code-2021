e50b6758425d0e2a9c751a4f16d735f50ca9c052
Timothy Diguiet <timothy.diguiet@epitech.eu>
2021-12-04 02:27:18 +0100

    Add day 3 solution

--- src/main/java/com/diguiet/adventofcode2021/day3/Day3.java
+++ src/main/java/com/diguiet/adventofcode2021/day3/Day3.java
@@ -4,7 +4,9 @@ import com.diguiet.adventofcode2021.utils.ResourceForDay;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class Day3 {
@@ -18,12 +20,48 @@ public class Day3 {
 
     public static void partOne() {
         final List<String> instructions = ResourceForDay.getInputAsList(DAY);
-        log.info("Result: " + 0);
+        final int nbBits = instructions.get(0).length();
+        final int[] bitAtZero = new int[nbBits];
+        final int majority = instructions.size() / 2;
+        for (String instruction : instructions) {
+            for (int i = 0; i < nbBits; i++) {
+                if (instruction.charAt(i) == '0') {
+                    ++bitAtZero[i];
+                }
+            }
+        }
+        String gamma = "";
+        for (int i = 0; i < nbBits; i++) {
+            gamma = gamma.concat((bitAtZero[i] > majority) ? "1" : "0");
+        }
+        // flip gamma bits
+        final String epsilon = gamma.chars().mapToObj(c -> ((c == '1') ? "0" : "1")).collect(Collectors.joining());
+        log.info("Result: " + Integer.parseInt(gamma, 2) * Integer.parseInt(epsilon, 2));
     }
 
     public static void partTwo() {
         final List<String> instructions = ResourceForDay.getInputAsList(DAY);
+        final int oxygen = getRating(instructions, true);
+        final int co2 = getRating(instructions, false);
 
-        log.info("Result: " + 0);
+        log.info("Result: " + oxygen * co2);
+    }
+
+    private static int getRating(final List<String> instructions, boolean isBasedOnMostCommonValue) {
+        List<String> ratingFiltered = new ArrayList<>(instructions);
+        int bitPos = 0;
+        while (ratingFiltered.size() > 1) {
+            final int finalBitPos = bitPos++;
+            int bitsSetToOne = (int) ratingFiltered
+                    .stream()
+                    .filter(s -> s.charAt(finalBitPos) == '1')
+                    .count();
+            final boolean isOneMostCommon = bitsSetToOne * 2 >= ratingFiltered.size();
+            ratingFiltered = ratingFiltered
+                    .stream()
+                    .filter((s) -> s.charAt(finalBitPos) == ((isOneMostCommon == isBasedOnMostCommonValue) ? '1' : '0'))
+                    .collect(Collectors.toList());
+        }
+        return Integer.parseInt(ratingFiltered.get(0), 2);
     }
 }
