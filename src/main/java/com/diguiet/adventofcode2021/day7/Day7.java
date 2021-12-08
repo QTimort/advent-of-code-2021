48c4cea3e8770314b00f9a2a0ac8d09cc8b630a9
Timothy Diguiet <timothy.diguiet@epitech.eu>
2021-12-08 05:40:07 +0100

    Add day 7

---
+++ src/main/java/com/diguiet/adventofcode2021/day7/Day7.java
@@ -0,0 +1,29 @@
+package com.diguiet.adventofcode2021.day7;
+
+import com.diguiet.adventofcode2021.utils.ResourceForDay;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class Day7 {
+    private static final int DAY = -1;
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        partOne();
+        partTwo();
+    }
+
+    public static void partOne() {
+        final List<String> instructions = ResourceForDay.getInputAsList(DAY);
+        log.info("Result: " + 0);
+    }
+
+    public static void partTwo() {
+        final List<String> instructions = ResourceForDay.getInputAsList(DAY);
+
+        log.info("Result: " + 0);
+    }
+}
---
+++ src/main/java/com/diguiet/adventofcode2021/day7/problem.md
@@ -0,0 +1,7 @@
+# Day 7: Title
+**https://adventofcode.com/2021/day/7**
+
+## Part One
+
+## Part Two
+
